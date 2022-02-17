9d91df3b9d6dc735f56ab841c919fd6f2dce4d6c
RepositorioIECA <99725187+RepositorioIECA@users.noreply.github.com>
2022-02-17 12:31:14 +0100

    Incorporación de la Páginas IASS
    Se acaba de crear el respositorio y se han cargado todos los ficheros que configuran este producto WEB

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$color       = ESTAD;
+$tipo        = PAG_PRODUCTO;
+$titulo      = "Indicadores de Actividad del Sector Servicios";
+$titulo_en   = "Activity Indicators in the Services Sector";
+$descripcion = "La operación Indicadores de Actividad del Sector Servicios tiene como principal objetivo la obtención de información sobre la evolución de los servicios de mercado en Andalucía (Comercio, Turismo, Transporte, Tecnologías de la Información y Servicios a empresas) mediante el seguimiento de dos variables, la cifra de negocios y el empleo";
+$caminomigas = caminoMigas('est','comercio');
+?>
